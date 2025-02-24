42915e044b8060c4210de1a2ed9e6c77cd34f74a
svetozarov <126297582+svetozarov@users.noreply.github.com>
2025-02-24 21:47:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="AS_GeminiCaptioning",
-    version="0.1.0",
+    version="1.0.0",
     description="Node for ComfyUI that builds a multimodal prompt from text inputs and an image, sends it to Google Gemini via the google-generativeai SDK, and returns the textual response.",
     author_email="art.svetozarov@gmail.com",
-    url="https://github.com/svetozarov/AS_GeminiCaptioning/", 
+    url="https://github.com/svetozarov/AS_GeminiCaptioning", 
     packages=find_packages(),
     install_requires=[
         "Pillow",
